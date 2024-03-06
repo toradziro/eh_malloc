f668657596c76579e0e6d2ef759bcbd7c58ad8c0
toradziro <toradziro@gmail.com>
2024-03-06 09:37:52 +0100

    Added Structure For Global Heap

--- inc/border_tags_allocator.h
+++ inc/border_tags_allocator.h
@@ -1,5 +1,27 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdbool.h>
+
+typedef struct SBlockFooter
+{
+    int     m_blockSize;
+    bool    m_isFree;
+} BlockFooter;
+
+typedef struct SBlockHeader
+{
+    int     m_blockSize;
+    bool    m_isFree;
+} BlockHeader;
+
+typedef struct SHeap
+{
+    void*           m_buffer;
+    BlockHeader*    m_firstBlock;
+    BlockFooter*    m_lastFooter;
+    int             m_bufferSize;
+    int             m_freeSpace;
+} BTagsHeap;
 
 void setupBTagsAllocator(void *buf, size_t size);
